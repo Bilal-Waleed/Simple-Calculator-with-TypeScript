6e8c2dce7840613df519f5d5ea764c14fc9bb859
bilal-waleed <bilalwaleed@246gmail.com>
2024-03-17 18:24:54 -0700

    Typescript project

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your First number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"],
+    }
+]);
+//conditional statments
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBSTRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    "Please select valid operator";
+}
